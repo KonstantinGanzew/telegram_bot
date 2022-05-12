a08a852ee40a1fb383033d8a760d8a20b6b3ddb3
KonstantinGanzew <36606021+KonstantinGanzew@users.noreply.github.com>
2022-05-12 18:54:22 +0500

    Update create_bot.py

--- telebot/create_bot.py
+++ telebot/create_bot.py
@@ -3,5 +3,5 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import os
 
-bot = Bot('1810556658:AAHPm_ngxFX3CyYXohrUQ_AB4_XwjgzzB8I')
+bot = Bot()
 dp = Dispatcher(bot, storage=MemoryStorage())
