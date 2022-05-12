960322196645e92c368a9daf5742d89638586a58
Konstantine Gantcev <kanzew@gmail.com>
2022-05-12 19:00:24 +0500

    скрыл файл

--- telebot/create_bot.py
+++ telebot/create_bot.py
@@ -3,10 +3,6 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import config
 
-<<<<<<< HEAD
 bot = Bot(config.TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
-=======
-bot = Bot()
-dp = Dispatcher(bot, storage=MemoryStorage())
->>>>>>> a08a852ee40a1fb383033d8a760d8a20b6b3ddb3
+
