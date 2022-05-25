a026a9b54708f18c7d6a80d9c1fb71e93be5d5fa
Константин Ганцев <kanzew@gmail.com>
2022-05-25 11:05:57 +0500

    Поправил данные в ебида

--- telebot/handlers/EBITDA.py
+++ telebot/handlers/EBITDA.py
@@ -3,7 +3,7 @@ from aiogram.dispatcher.filters import Text
 from create_bot import dp, bot
 import requests
 
-url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEhyDJiwtbnqI_Oj1olArtSfa-sbpLh7YAXKraR2bfLf1mKrsCuIhS_uWBXd4rkovJGurjKGWeB9wl/pub?gid=1524050779&single=true&output=pdf"
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSdtH67ClEw3Jnu9U1PAeqTZZaWPY_On8BMZYIfexIi7Atn7LHyb1rMMg8LBmzgvEx3sALz3SL_vQQX/pub?gid=378550121&single=true&output=pdf"
 headers = {"user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"}
 
 @dp.message_handler(Text(equals='EBITDA'))
